cd7e53b38dc4bfdb2e018ff0154596244b511749
Tim <72320428+timkatimz@users.noreply.github.com>
2022-08-01 17:48:43 +0400

    Update config.py

--- project/config.py
+++ project/config.py
@@ -11,10 +11,10 @@ class BaseConfig:
 
 class TestingConfig(BaseConfig):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://timz:tim@127.0.0.1/flask_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://flask_app:tim@127.0.0.1/flask_db"
 
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
     SQLALCHEMY_ECHO = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://timz:tim@127.0.0.1/flask_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://flask_app:tim@127.0.0.1/flask_db"
