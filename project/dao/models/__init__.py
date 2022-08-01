34a36f69589f030d57054fce41e7e630653a8749
Tim <72320428+timkatimz@users.noreply.github.com>
2022-08-01 17:32:35 +0400

    Update __init__.py

--- project/dao/models/__init__.py
+++ project/dao/models/__init__.py
@@ -1,9 +1,11 @@
 from .genre import Genre
 from .movie import Movie
 from .director import Director
+from .user import User
 
 __all__ = [
     "Genre",
     "Movie",
     "Director",
+    "User"
 ]
